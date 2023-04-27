7e7ba4cd2a84bd1f46d980d1a4a4534d83189898
Andryss <andr.krivosheev@gmail.com>
2023-04-27 11:41:18 +0300

    Add simple test

---
+++ src/test/java/model/AreaCheckerTest.java
@@ -0,0 +1,35 @@
+package model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AreaCheckerTest {
+
+    @Test
+    public void testCheck() {
+        AreaChecker checker = AreaChecker.instance;
+
+        for (double r = 0.5; r < 10; r += 0.5) {
+            // Out of the area
+            assertFalse(checker.check(new Request(0.1 * r, 0.1 * r, r)));
+            assertFalse(checker.check(new Request(-0.5 * r, -0.5 * r, r)));
+            assertFalse(checker.check(new Request(-0.3 * r, 0.3 * r, r)));
+
+            // In the area
+            assertTrue(checker.check(new Request(-0.3 * r, -0.3 * r, r)));
+            assertTrue(checker.check(new Request(-0.25 * r, 0.25 * r, r)));
+            assertTrue(checker.check(new Request(0 * r, 0 * r, r)));
+
+            // Borders
+            assertTrue(checker.check(new Request(-0.5 * r, 0 * r, r)));
+            assertTrue(checker.check(new Request(0 * r, 0.5 * r, r)));
+            assertTrue(checker.check(new Request(0 * r, 0 * r, r)));
+            assertTrue(checker.check(new Request(0.5 * r, 0 * r, r)));
+            assertTrue(checker.check(new Request(0.5 * r, -1 * r, r)));
+            assertTrue(checker.check(new Request(0 * r, -1 * r, r)));
+            assertTrue(checker.check(new Request(0 * r, -0.5 * r, r)));
+        }
+    }
+
+}
