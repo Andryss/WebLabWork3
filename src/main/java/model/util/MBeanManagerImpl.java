79b72e100437020351d0be43ca9aa6e210adb100
Andryss <andr.krivosheev@gmail.com>
2023-05-15 22:19:13 +0300

    Add MBeanManager

---
+++ src/main/java/model/util/MBeanManagerImpl.java
@@ -0,0 +1,42 @@
+package model.util;
+
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+import javax.management.*;
+import java.lang.management.ManagementFactory;
+import java.util.HashMap;
+import java.util.Map;
+
+@ManagedBean(name = "mBeanManager")
+@ApplicationScoped
+public class MBeanManagerImpl implements MBeanManager {
+
+    private final Map<Object, ObjectName> objectNameMap = new HashMap<>();
+    private final MBeanServer mBeanServer = ManagementFactory.getPlatformMBeanServer();
+
+    @Override
+    public void registerMBean(String beanName, Object bean) {
+        try {
+            String domain = bean.getClass().getPackage().getName();
+            String type = bean.getClass().getSimpleName();
+            ObjectName objectName = new ObjectName(String.format("%s:type=%s,name=%s", domain, type, beanName));
+            mBeanServer.registerMBean(bean, objectName);
+            objectNameMap.put(bean, objectName);
+        } catch (MalformedObjectNameException | NotCompliantMBeanException | InstanceAlreadyExistsException |
+                 MBeanRegistrationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void unregisterMBean(Object bean) {
+        try {
+            ObjectName objectName = objectNameMap.get(bean);
+            if (objectName == null)
+                throw new IllegalArgumentException("Bean is not registered: " + bean);
+            mBeanServer.unregisterMBean(objectName);
+        } catch (InstanceNotFoundException | MBeanRegistrationException e) {
+            e.printStackTrace();
+        }
+    }
+}
